2d0fe9b74c463712f6610c4fc42a7c549eaa77d9
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-03-16 10:39:50 +0100

    Add python package inotify-simple

--- default.nix
+++ default.nix
@@ -7,21 +7,29 @@ let
   # the tests as 1 fail (because it missed 1 file in the pypi package)
   # issue: https://github.com/supermihi/pytaglib/pull/46
   # pr to fix it: https://github.com/supermihi/pytaglib/pull/56
-  self = rec {
+  self = let packages = pkgs.python36Packages;
+  in rec {
     # if this is used a dependency for another haskellPackage you
     # will need to redefine "pkgs.haskellPackages.callPackage",
     # but for simple cases this is sufficient.
     # jot = pkgs.haskellPackages.callPackage /home/amy/jot/jot.nix { };
     # foo = callPackage /path/to/foo { }; # where directory foo contains a default.nix
     # harold = callPackage /path/to/harold/harold.nix { };
+    # dependency for ardumont-pytools
     pyexifinfo = pkgs.callPackage ./python/pyexifinfo/release.nix {
-      buildPythonPackage = pkgs.python36Packages.buildPythonPackage;
-      fetchPypi = pkgs.python36Packages.fetchPypi;
+      buildPythonPackage = packages.buildPythonPackage;
+      fetchPypi = packages.fetchPypi;
     };
     ardumont-pytools = pkgs.callPackage ./python/ardumont-pytools/release.nix {
-      pkgs = pkgs.python36Packages;
-      buildPythonPackage = pkgs.python36Packages.buildPythonPackage;
+      pkgs = packages;
+      buildPythonPackage = packages.buildPythonPackage;
       pyexifinfo = pyexifinfo;
+      inotify-tools = pkgs.inotify-tools;
+    };
+    # dependency for xkeysnail
+    inotify-simple = pkgs.callPackage ./python/inotify_simple/release.nix {
+      buildPythonPackage = packages.buildPythonPackage;
+      fetchPypi = packages.fetchPypi;
       inotify-tools = pkgs.inotify-tools;
     };
   };
---
+++ python/inotify_simple/release.nix
@@ -0,0 +1,25 @@
+{ lib, buildPythonPackage, fetchPypi, inotify-tools }:
+
+buildPythonPackage rec {
+  pname="inotify_simple";
+  version = "1.1.8";
+
+   src = fetchPypi {
+     inherit pname version;
+     sha256 = "1pfqvnynwh318cakldhg7535kbs02asjsgv6s0ki12i7fgfi0b7w";
+   };
+
+   propagatedBuildInputs = [ inotify-tools ];
+   doCheck = false;  # no test
+
+   meta = {
+     homepage = https://github.com/chrisjbillington/inotify_simple;
+     description = ''
+       inotify_simple is a simple Python wrapper around
+       inotify. No fancy bells and whistles, just a literal wrapper
+       with ctypes. Only 122 lines of code!
+     '';
+     license = lib.licenses.bsdOriginal;
+     maintainers = with lib.maintainers; [ ardumont ];
+  };
+}
