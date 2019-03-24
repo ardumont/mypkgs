ae67e7b0ae04dab5caabbdcc3a64ab64da06ef3a
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-03-24 18:11:39 +0100

    default: Reduce the scope to where it's needed

--- default.nix
+++ default.nix
@@ -10,32 +10,6 @@ let
   packages = pkgs.python36Packages;
   # nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
   # nix-channel --update
-  unstable = import <nixpkgs-unstable> {};
-  # build specifically this override with a nixpkgs-unstable version
-  myPythonOverride = unstable.pkgs.python36Packages.override {
-    overrides = self: super: {
-      urwidtrees = super.urwidtrees.overrideAttrs (old: rec {
-        version = "1.0.3.dev0";
-        src = packages.fetchPypi {
-          pname = old.pname;
-          version = version;
-          sha256 = "05sf9q5lvcazccwzxc4zl2xnxfds7sb9yis18z99p01nrljp868r";
-        };
-        propagatedBuildInputs = old.propagatedBuildInputs ++ [ packages.mock ];
-      });
-      urwid = super.urwid.overrideAttrs (old: rec {
-        version = "2.0.1";
-        src = packages.fetchPypi {
-          pname = old.pname;
-          version = version;
-          sha256 = "1g6cpicybvbananpjikmjk8npmjk4xvak1wjzji62wc600wkwkb4";
-        };
-        patches = [];
-        doCheck = false;
-        # propagatedBuildInputs = old.propagatedBuildInputs ++ [ packages.mock ];
-      });
-    };
-  };
   self = rec {
     # if this is used a dependency for another haskellPackage you
     # will need to redefine "pkgs.haskellPackages.callPackage",
@@ -72,11 +46,40 @@ let
       buildPythonPackage = packages.buildPythonPackage;
       fetchPypi = packages.fetchPypi;
     };
-    stig = pkgs.callPackage ./python/stig/release.nix {
-      pkgs = myPythonOverride;
-      buildPythonPackage = myPythonOverride.buildPythonPackage;
-      fetchPypi = myPythonOverride.fetchPypi;
-      inherit async_http;
+    stig =
+    let # nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
+        # nix-channel --update
+      unstable = import <nixpkgs-unstable> {};
+      # build specifically this override with a nixpkgs-unstable version
+      myPythonOverride = unstable.pkgs.python36Packages.override {
+        overrides = self: super: {
+          urwidtrees = super.urwidtrees.overrideAttrs (old: rec {
+            version = "1.0.3.dev0";
+            src = packages.fetchPypi {
+              pname = old.pname;
+              version = version;
+              sha256 = "05sf9q5lvcazccwzxc4zl2xnxfds7sb9yis18z99p01nrljp868r";
+            };
+            propagatedBuildInputs = old.propagatedBuildInputs ++ [ packages.mock ];
+          });
+          urwid = super.urwid.overrideAttrs (old: rec {
+            version = "2.0.1";
+            src = packages.fetchPypi {
+              pname = old.pname;
+              version = version;
+              sha256 = "1g6cpicybvbananpjikmjk8npmjk4xvak1wjzji62wc600wkwkb4";
+            };
+            patches = [];
+            doCheck = false;
+            # propagatedBuildInputs = old.propagatedBuildInputs ++ [ packages.mock ];
+          });
+        };
+      };
+      in pkgs.callPackage ./python/stig/release.nix {
+          pkgs = myPythonOverride;
+          buildPythonPackage = myPythonOverride.buildPythonPackage;
+          fetchPypi = myPythonOverride.fetchPypi;
+          inherit async_http;
     };
   };
 in self
