c926a7f4d73ad19738bfeb8ac8ac378871e5f86c
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2020-03-29 17:59:57 +0200

    Revert "emacs: Give anzu mode a spin"
    This reverts commit 31580882b1d0f42d355887d7973e8ec6c4db4d41.
    
    It's already in nixpkgs!

--- default.nix
+++ default.nix
@@ -5,7 +5,6 @@ let
   python-packages = pkgs.python3Packages;
   self = rec {
     emacs-powerline = pkgs.callPackage ./emacs/emacs-powerline/release.nix { };
-    emacs-anzu = pkgs.callPackage ./emacs/emacs-anzu/release.nix { };
     # dependency for ardumont-pytools
     pyexifinfo = pkgs.callPackage ./python/pyexifinfo/release.nix {
       pkgs = python-packages;
