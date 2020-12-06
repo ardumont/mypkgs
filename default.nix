de68b562064d8316f2cecccfffa31c7fe5fee9bd
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2020-12-06 18:14:40 +0100

    Convert to use flake

--- default.nix
+++ default.nix
@@ -1,67 +1,63 @@
-{ system ? builtins.currentSystem, sources ? import ./nix/sources.nix, ... }:
+{ pkgs, ... }:
 
-let
-  pkgs = import sources.nixpkgs { inherit system; };
-  pkgs-unstable = import sources.nixpkgs-unstable { inherit system; };
-  python-packages = pkgs.python3Packages;
-  self = rec {
-    fmt = pkgs-unstable.fmt;
+let python-packages = pkgs.python3Packages;
+in rec {
+  fmt = pkgs.fmt;
 
-    spdlog = pkgs-unstable.spdlog.overrideAttrs (oldAttrs: rec {
-      cmakeFlags = oldAttrs.cmakeFlags ++ [
-        "-DSPDLOG_FMT_EXTERNAL=ON"
-      ];
+  spdlog = pkgs.spdlog.overrideAttrs (oldAttrs: rec {
+    cmakeFlags = oldAttrs.cmakeFlags ++ [
+      "-DSPDLOG_FMT_EXTERNAL=ON"
+    ];
 
-      buildInputs = [
-        pkgs-unstable.fmt.dev
-        pkgs.pkg-config pkgs.systemd
-      ];
-    });  # ok
+    buildInputs = [
+      pkgs.fmt.dev
+      pkgs.pkg-config pkgs.systemd
+    ];
+  });  # ok
 
-    emacs-powerline = pkgs.callPackage ./emacs/emacs-powerline/release.nix { };
-    # dependency for ardumont-pytools
-    watchdog-gevent = pkgs.callPackage ./python/watchdog-gevent/release.nix {
-      pkgs = python-packages;
-    };
-    # dependency for xkeysnail
-    inotify-simple = pkgs.callPackage ./python/inotify_simple/release.nix {
-      pkgs = python-packages;
-    };
-    xkeysnail = pkgs.callPackage ./python/xkeysnail/release.nix {
-      pkgs = python-packages;
-      inotify-simple = inotify-simple;
-    };
-    async_http = pkgs.callPackage ./python/async_http/release.nix {
-      pkgs = python-packages;
-    };
-    stig = let
-      my-python-override = python-packages.override {
-        overrides = self: super: {
-          urwidtrees = super.urwidtrees.overrideAttrs (old: rec {
-            version = "1.0.3.dev0";
-            src = python-packages.fetchPypi {
-              pname = old.pname;
-              version = version;
-              sha256 = "05sf9q5lvcazccwzxc4zl2xnxfds7sb9yis18z99p01nrljp868r";
-            };
-            propagatedBuildInputs = old.propagatedBuildInputs ++ [ python-packages.mock ];
-          });
-          urwid = super.urwid.overrideAttrs (old: rec {
-            version = "2.0.1";
-            src = python-packages.fetchPypi {
-              pname = old.pname;
-              version = version;
-              sha256 = "1g6cpicybvbananpjikmjk8npmjk4xvak1wjzji62wc600wkwkb4";
-            };
-            patches = [];
-            doCheck = false;
-            # propagatedBuildInputs = old.propagatedBuildInputs ++ [ python-packages.mock ];
-          });
-        };
-      };
-    in pkgs.callPackage ./python/stig/release.nix {
-      pkgs = my-python-override;
-      inherit async_http;
-    };
+  emacs-powerline = pkgs.callPackage ./emacs/emacs-powerline/release.nix { };
+  # dependency for ardumont-pytools
+  watchdog-gevent = pkgs.callPackage ./python/watchdog-gevent/release.nix {
+    pkgs = python-packages;
   };
-in self
+  # dependency for xkeysnail
+  inotify-simple = pkgs.callPackage ./python/inotify_simple/release.nix {
+    pkgs = python-packages;
+  };
+  xkeysnail = pkgs.callPackage ./python/xkeysnail/release.nix {
+    pkgs = python-packages;
+    inotify-simple = inotify-simple;
+  };
+  async_http = pkgs.callPackage ./python/async_http/release.nix {
+    pkgs = python-packages;
+  };
+  stig = let
+    my-python-override = python-packages.override {
+      overrides = self: super: {
+        urwidtrees = super.urwidtrees.overrideAttrs (old: rec {
+          version = "1.0.3.dev0";
+          src = python-packages.fetchPypi {
+            pname = old.pname;
+            version = version;
+            sha256 = "05sf9q5lvcazccwzxc4zl2xnxfds7sb9yis18z99p01nrljp868r";
+          };
+          propagatedBuildInputs = old.propagatedBuildInputs ++ [ python-packages.mock ];
+        });
+        urwid = super.urwid.overrideAttrs (old: rec {
+          version = "2.0.1";
+          src = python-packages.fetchPypi {
+            pname = old.pname;
+            version = version;
+            sha256 = "1g6cpicybvbananpjikmjk8npmjk4xvak1wjzji62wc600wkwkb4";
+          };
+          patches = [];
+          doCheck = false;
+          # propagatedBuildInputs = old.propagatedBuildInputs ++ [ python-packages.mock ];
+        });
+      };
+    };
+  in pkgs.callPackage ./python/stig/release.nix {
+    pkgs = my-python-override;
+    inherit async_http;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Mypkgs flake";
+
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixos-20.03";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    with nixpkgs.lib;
+    let supportedSystems = [
+          "x86_64-linux"
+          "i686-linux"
+          "aarch64-linux"
+        ];
+        forEachSystem = f: genAttrs supportedSystems (system: f system);
+        pkgsBySystem = forEachSystem (system:
+          import nixpkgs {
+            inherit system;
+            # overlays = self.internal.overlays."${system}";
+          }
+        );
+    in {
+      # internals = {
+      #   overlays = forEachSystem (system: [
+      #     (self.overlay."${system}")
+      #     # Add other overlays if any
+      #     # ...
+      #   ]);
+      # };
+
+      # overlay = forEachSystem (system: _: _: self.packages."${system}");
+
+      packages = forEachSystem (system:
+        let pkgs = pkgsBySystem."${system}";
+            all-pkgs = (import ./default.nix { inherit pkgs; });
+        in {
+          emacs-powerline = all-pkgs.emacs-powerline;
+          fmt = all-pkgs.fmt;
+          spdlog = all-pkgs.spdlog;
+          watchdog-gevent = all-pkgs.watchdog-even;
+          inotify-simple = all-pkgs.inotify-simple;
+          xkeysnail = all-pkgs.xkeysnail;
+          async_http = all-pkgs.async_http;
+          stig = all-pkgs.stig;
+        }
+      );
+
+      defaultPackage = forEachSystem (system: self.packages."${system}".emacs-powerline);
+    };
+}
